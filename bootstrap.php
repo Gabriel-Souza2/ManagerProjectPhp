f7419a498d4edd765b2449ec2483c383410d3226
Gabriel-Souza2 <gabriel.souza2@hotmail.com.br>
2019-05-20 19:19:51 -0300

    Adicionando Eventos

--- bootstrap.php
+++ bootstrap.php
@@ -1,13 +1,14 @@
 <?php
 require  __DIR__.'/vendor/autoload.php';
-require __DIR__.'/config/containers.php';
+
 
 
 use SON\Framework\Router;
 use SON\Framework\Response;
 
 $router = new Router();
-
+require __DIR__.'/config/containers.php';
+require __DIR__.'/config/events.php';
 require __DIR__.'/config/routes.php';
 
 try
